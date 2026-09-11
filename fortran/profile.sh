bd886d9f6615c942297ce29ba11ebf84f81d75a6
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2026-09-11 16:10:03 +0100

    Add simple profiling script

---
+++ fortran/profile.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+bin=$1
+bname=$(basename "$bin")
+repname="report_$bname"
+
+if test ! -f "$repname.nsys-rep"; then
+  echo "Profiling $bin"
+  echo
+  nsys profile -o "$repname" --force-overwrite=true "$bin" -n 10000
+else
+  echo "Skipping profile. To re-run:"
+  echo "rm $repname.nsys-rep"
+  echo
+fi
+
+nsys stats --force-export=true --report cuda_gpu_kern_sum,cuda_gpu_mem_time_sum "$repname.nsys-rep"
